c0c61bd0ffb5edb1ab6095455dda2cee20f16d18
Vedant Pandey <vedantpandey46@gmail.com>
2023-07-04 21:19:23 +0530

    day 1 p2 soln

--- day1/solve.go
+++ day1/solve.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -11,19 +12,20 @@ func Solve() {
 	file, _ := os.Open("./day1/input.txt")
 	defer file.Close()
 	scnr := bufio.NewScanner(file)
-	maxCal := 0
+	var calo []int
 	curCal := 0
 	for scnr.Scan() {
 		line := scnr.Text()
 		if line == "" {
-			if curCal > maxCal {
-				maxCal = curCal
-			}
+			calo = append(calo, curCal)
 			curCal = 0
 		} else {
 			val, _ := strconv.Atoi(line)
 			curCal += val
 		}
 	}
-	fmt.Println(maxCal)
+	sort.Ints(calo)
+	sum := calo[len(calo)-3] + calo[len(calo)-2] + calo[len(calo)-1]
+
+	fmt.Println(sum)
 }
